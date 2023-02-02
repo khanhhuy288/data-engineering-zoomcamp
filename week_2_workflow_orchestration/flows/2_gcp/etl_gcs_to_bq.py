28d183f3dac301a623660974f11d6d9c391329f7
Huy Tran <khanhhuy288@gmail.com>
2023-02-02 21:01:41 +0100

    Deployed the parameterized etl_web_to_gcs flow in a Docker container

--- week_2_workflow_orchestration/flows/2_gcp/etl_gcs_to_bq.py
+++ week_2_workflow_orchestration/flows/2_gcp/etl_gcs_to_bq.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+
 import pandas as pd
 from prefect import flow, task
-from prefect_gcp.cloud_storage import GcsBucket
 from prefect_gcp import GcpCredentials
+from prefect_gcp.cloud_storage import GcsBucket
 
 
 @task(retries=3)
@@ -10,7 +11,7 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
     """Download trip data from GCS"""
     gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
     gcs_block = GcsBucket.load("zoom-gcs")
-    gcs_block.get_directory(from_path=gcs_path, local_path=f"../")
+    gcs_block.get_directory(from_path=gcs_path, local_path=f"../../")
     return Path(f"../{gcs_path}")
 
 
